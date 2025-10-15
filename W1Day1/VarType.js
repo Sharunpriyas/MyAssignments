4ab72ef5d8b079026005d028de36e462d14fbb73
Sharunpriyas <sharunpriyas@gmail.com>
2025-10-16 01:33:46 +0530

    Add files via upload
    Day1

---
+++ W1Day1/VarType.js
@@ -0,0 +1,24 @@
+// Varibale type
+
+var firstName = "Sharun"
+console.log("firstName:" +firstName)
+console.log(typeof(firstName))
+
+var companyName = "TestLeaf"
+console.log("Company name is:" +companyName)
+console.log(typeof(companyName))
+
+var mobileNumber = 7896545462n
+console.log("Mobile Number is:" +mobileNumber)
+console.log(typeof(mobileNumber))
+
+var isAutomation = true
+console.log("is Automation:" +isAutomation)
+console.log(typeof(isAutomation))
+
+var hasPlaywright 
+console.log("Not Defined" +hasPlaywright)
+console.log(typeof(hasPlaywright))
+
+
+
